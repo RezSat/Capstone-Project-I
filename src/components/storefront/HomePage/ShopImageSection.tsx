061af95981a95a7e5a6987fc52c89c1b1f9cb21d
RezSat <yehantest@gmail.com>
2026-06-24 17:45:25 +0530

    Remove unused components, images, and dead code
    - Delete 16 unused HomePage components (BestSellingProducts, BrandQuoteSection, FeaturedCategories, HeroBackgroundCarousel, HeroSection, HomeVideoSection, NewArrivals, PlayersEdge, PlayerTipsSlider, PurposeMissionSection, ShopImageSection, SmashMastersPicks, SportMatcher, SportMatcherMobile, SportsCategorySlider, StoreBenefitsBar)
    - Delete 11 unused common components (BrandButton family, BlockTextReveal, BlockTextRevealModified, RollingText)
    - Delete 4 unused data files (player-tips, sports-category-slider-data, sportMatcher, footer)
    - Delete 20 unused public assets (logo, sample, SVGs, payment icons, social icons, product images, video)
    - Remove dead products array and getProductBySlug from products.ts (types retained)
    - Only main branch remains

--- src/components/storefront/HomePage/ShopImageSection.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function ShopImageSection() {
-  return (
-    <section className="relative h-[300px] w-full overflow-hidden">
-      <Image src="/images/storefront-generic/checkout-packages.png" alt="Generic ecommerce package display" fill className="object-cover" />
-    </section>
-  )
-}
